9af8b99d212a7aacff2aaef55a70fa4922018235
IharTsykaka <35345292+IharTsykaka@users.noreply.github.com>
2018-02-11 23:42:29 +0300

    Test Tsykala

--- src/by/it/Tsykala/_tasks_/lesson02/TaskC1.java
+++ src/by/it/Tsykala/_tasks_/lesson02/TaskC1.java
@@ -9,10 +9,18 @@ package by.it.Tsykala._tasks_.lesson02;
 Вывод:
 Sum = 26
 */
+import java.util.Scanner;
+
 class TaskC1 {
-
-
-
-
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        int j = sc.nextInt();
+        int result = i + j;
+        System.out.println("Sum" + " = " + result);
+    }
 
 }
+
+
+
