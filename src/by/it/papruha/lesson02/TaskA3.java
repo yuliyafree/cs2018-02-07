b9d7a51d18a63e911c56847a84dcadba90c56f83
AlenaPV <poprugo@gmail.com>
2018-02-12 11:23:54 +0300

    lesson2 complete A B and C1

---
+++ src/by/it/papruha/lesson02/TaskA3.java
@@ -0,0 +1,15 @@
+package by.it.papruha.lesson02;
+/*
+Измените переменные и строку вывода в программе так,
+чтобы она рассчитывала и печатала следующее выражение
+3*3+4*4=25
+ */
+
+class TaskA3 {
+    public static void main(String[] args) {
+        int i = 4;
+        int j = 3;
+        int k = i*i + j*j;
+        System.out.println(j+"*"+j+"+"+i+"*"+i+"="+k);
+    }
+}
