d6e076fc4cba891046946d2a62831661d0ba7dc4
IharTsykaka <35345292+IharTsykaka@users.noreply.github.com>
2018-02-14 22:58:34 +0300

    les 04 A B2

---
+++ src/by/it/_examples_/Tsykala/_tasks_/lesson02/TaskA1.java
@@ -0,0 +1,10 @@
+package by.it._examples_.Tsykala._tasks_.lesson02;
+
+class TaskA1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+
+
---
+++ src/by/it/_examples_/Tsykala/_tasks_/lesson02/TaskC2.java
@@ -0,0 +1,33 @@
+package by.it._examples_.Tsykala._tasks_.lesson02;
+
+import java.util.Scanner;
+
+/* Нужно написать программу, которая вводит два числа с клавиатуры
+и 4 раза выводит их сумму с обозначением системы счисления на экран в
+ДЕСЯТИЧНОМ ДВОИЧНОМ ШЕСТНАДЦАТИРИЧНОМ ВОСЬМИРИЧНОМ виде
+
+Вот пример ввода с клавиатуры:
+34 26
+
+Тогда вывод ожидается такой:
+DEC:34+26=60
+BIN:100010+11010=111100
+HEX:22+1a=3c
+OCT:42+32=74
+*/
+class TaskC2 {
+   /* public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        byte i = sc.nextByte();
+        byte j = sc.nextByte();
+
+        String binary = Integer.toBinaryString(i);
+        if (binary.length() > 8) binary = binary.substring(binary.length() - 8);
+        binary = String.format("%8s", binary).replace(" ", "0");
+        //System.out.printf("%8s %4d %n", binary, i);
+        String binary = Integer.toBinaryString(j);
+        if (binary.length() > 8) binary = binary.substring(binary.length() - 8);
+        binary = String.format("%8s", binary).replace(" ", "0");
+        System.out.printf("%8s %4d %n", binary, i + j);*/
+    }
+
