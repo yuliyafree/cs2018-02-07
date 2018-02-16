faa5243ffcc00f7e887096b2d71026db81f9d425
AstapovichIlya <astapovich.il.an@gmail.com>
2018-02-16 03:09:30 +0300

    lesson4 A,B,C(1);

---
+++ src/by/it/astapovich/lesson04/TaskB3.java
@@ -0,0 +1,47 @@
+package by.it.astapovich.lesson04;
+/*
+С клавиатуры вводится некоторое число.
+Пусть считается сумма int sum=1+2+3+4+5+... и т.д.
+Каждая цифра суммы выводится в консоль через запятую (без пробела).
+Остановите вывод до того, как сумма превысит введенное число.
+
+Например:
+Ввод:
+122
+Вывод:
+1,3,6,10,15,21,28,36,45,55,66,78,91,105,120,
+
+Ввод:
+15
+Вывод:
+1,3,6,10,15,
+
+Ввод:
+14
+Вывод:
+1,3,6,10,
+
+
+ */
+
+
+import java.util.Scanner;
+
+public class TaskB3 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
+
+        for (int i = 1; i < a; i++) {
+            int sum = i * (i + 1) / 2;
+            if (sum > a) continue;
+            System.out.print(sum + ",");
+        }
+
+        System.out.print("");
+    }
+}
+
+
